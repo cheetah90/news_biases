c090c96114a70e1b6cee42ff32b87e6257c27666
Yilun Lin <cheetah0513@gmail.com>
2017-03-10 18:58:06 -0600

    Fix bugs

--- data_collection/download_wayback.py
+++ data_collection/download_wayback.py
@@ -2,7 +2,7 @@ import waybacktrack
 
 
 def main():
-    open("data_collection/foxnews_links.txt", 'w')
+    open("./data_collection/foxnews_links.txt", 'w')
     flinks, duds = waybacktrack.archive_domain(domain='www.foxnews.com/politics.html',
                                                year=2017,
                                                dir_path='foxnews/2017',
